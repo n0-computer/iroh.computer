60e87a5dd078b50d89d78447a84a8c667e4c634b
b5 <sparkle_pony_2000@qri.io>
2023-09-05 15:17:31 -0400

    quick fix for mobile menu on non-docs pages

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -231,11 +231,6 @@ export function Navigation(props) {
             className={groupIndex === 0 && 'md:mt-0'}
           />
         ))}
-        <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-          <Button href="#" variant="filled" className="w-full">
-            Sign in
-          </Button>
-        </li>
       </ul>
     </nav>
   );
