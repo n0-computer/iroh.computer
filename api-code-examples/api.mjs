0b999ad052765fc9fc57388178f6c36df141e53e
b5 <sparkle_pony_2000@qri.io>
2023-11-21 18:50:01 +0100

    update doc commands, remove setup instructions

--- api-code-examples/api.mjs
+++ api-code-examples/api.mjs
@@ -1,7 +1,179 @@
 
 const doc = [
   {
-    name: 'doc list',  
+    name: 'doc delete',
+    description: 'Delete all document entries below a key prefix.',
+    slug: 'doc-del',
+    arguments: [
+      { name: "prefix", necessity: 'required', description: "Prefix to delete. All entries whose key starts with or is equal to the prefix will be deleted" }
+    ],
+    examples: {
+      console: `> doc new --switch
+2aoukeibc2vdy5n2jlihnyv3e26cmketqbropptqfef3v7poe5eq
+Active doc is now 2aoukeibc2vdy5n2
+      
+author:luo73rdznvupzrjb doc:2aoukeibc2vdy5n2 
+> doc set key value
+bafkr4iagirfesfxon7wneztow6ila3w5mm53jnmcq4sek6nnnxxv7wk6bm
+
+author:luo73rdznvupzrjb doc:2aoukeibc2vdy5n2 
+> doc del key
+Deleting all entries whose key starts with key. Continue? yes
+Deleted 1 entries.
+Inserted an empty entry for author luo73rdznvupzrjb with key key`,
+    }
+  },
+  {
+    name: 'doc drop',
+    description: 'Delete an entire document from the local node',
+    slug: 'doc-drop',
+    arguments: [
+      { name: "doc", necessity: 'required', description: "Document to operate on." }
+    ],
+    examples: {
+      console: `> doc new --switch
+2aoukeibc2vdy5n2jlihnyv3e26cmketqbropptqfef3v7poe5eq
+Active doc is now 2aoukeibc2vdy5n2
+      
+author:luo73rdznvupzrjb doc:2aoukeibc2vdy5n2 
+> doc set key value
+bafkr4iagirfesfxon7wneztow6ila3w5mm53jnmcq4sek6nnnxxv7wk6bm
+
+> doc drop 2aoukeibc2vdy5n2jlihnyv3e26cmketqbropptqfef3v7poe5eq
+Deleting a document will permanently remove the document secret key, all document entries, 
+and all content blobs which are not referenced from other docs or tags.
+Delete document 2aoukeibc2vdy5n2? yes
+Doc 2aoukeibc2vdy5n2 has been deleted.`,
+    }
+  },
+  {
+    name: 'doc export',
+    description: 'Export the most recent data for a key from a document',
+    slug: 'doc-export',
+    examples: {
+      console: `> doc new --switch
+2aoukeibc2vdy5n2jlihnyv3e26cmketqbropptqfef3v7poe5eq
+Active doc is now 2aoukeibc2vdy5n2
+      
+author:luo73rdznvupzrjb doc:2aoukeibc2vdy5n2 
+> doc set key value
+bafkr4iagirfesfxon7wneztow6ila3w5mm53jnmcq4sek6nnnxxv7wk6bm
+
+> doc drop 2aoukeibc2vdy5n2jlihnyv3e26cmketqbropptqfef3v7poe5eq
+Deleting a document will permanently remove the document secret key, all document entries, 
+and all content blobs which are not referenced from other docs or tags.
+Delete document 2aoukeibc2vdy5n2? yes
+Doc 2aoukeibc2vdy5n2 has been deleted.`,
+    }
+  },
+  {
+    name: 'doc get',   
+    description: 'Get entries in a document.',
+    slug: 'doc-get',
+    arguments: [
+      { name: 'key', necessity: 'required', description: 'Key of the entry to fetch.' },
+      { name: 'doc_id', necessity: 'required', description: 'Required unless the document is set through the IROH_DOC environment variable. Within the Iroh console, the active document can also set with `doc set`.' },
+      { name: 'prefix', necessity: '', description: 'If true, get all entries that start with key.' },
+      { name: 'author', necessity: '', description: 'If provided, only return entries from this author.' },
+      { name: 'old', necessity: '', description: 'If true, old entries will be included. By default only the latest value for each key is shown.' },
+      { name: 'content', necessity: '', description: 'Also print the content for each entry (but only if smaller than 1MB and valid UTf-8).' }
+    ],
+    examples: {
+      console: `> doc new --switch
+dyyelvqqruxjwrlntsdvsksopwznmgnfxpehcsoqckklvqfxar2q
+Active doc is now dyyelvqq…
+
+author:fhu3uk4w… doc:dyyelvqq…
+> doc set foo bar
+@fhu3uk4w…: foo = 6lujp3wx… (3 B)
+
+author:fhu3uk4w… doc:dyyelvqq…
+> doc get foo -c
+@fhu3uk4w…: foo = 6lujp3wx… (3 B)
+bar
+
+`,
+    }
+  },
+  {
+    name: 'doc import',   
+    description: 'Import data into a document.',
+    slug: 'doc-import',
+    arguments: [
+      { name: 'path', necessity: 'required', description: 'Path to a local file or directory to import. Pathnames will be used as the document key' },
+      { name: 'doc', necessity: 'required', description: 'Document to operate on. Required unless the document is set through the IROH_DOC environment variable. Within the Iroh console, the active document can also be set with `doc switch`.' },
+      { name: 'author', necessity: 'required', description: 'Author of the entry. Required unless the author is set through the IROH_AUTHOR environment variable. Within the Iroh console, the active author can also be set with `author switch`.' },
+      { name: 'prefix', necessity: '', description: 'If true, get all entries that start with key.' },
+      { name: 'in place', necessity: '', description: ' If true, don\'t copy the file into iroh, reference the existing file instead, Moving a file imported with `in-place` will result in data corruption' },
+      
+    ],
+    examples: {
+      console: `> doc new --switch
+dyyelvqqruxjwrlntsdvsksopwznmgnfxpehcsoqckklvqfxar2q
+Active doc is now dyyelvqq…
+
+author:fhu3uk4w… doc:dyyelvqq…
+> doc import ~/foo.txt --prefix bar
+@fhu3uk4w…: bar/foo.txt = 6lujp3wx… (3 B)
+
+author:fhu3uk4w… doc:dyyelvqq…
+> doc get foo
+@fhu3uk4w…: foo = 6lujp3wx… (3 B)
+bar
+
+`,
+    }
+  },
+  {
+    name: 'doc join',  
+    description: 'Join a document from a ticket.',
+    slug: 'doc-join',
+    arguments: [
+      { name: 'ticket', necessity: 'required', description: 'The ticket to join a document. Create a ticket with \'doc share\'.' },
+      { name: 'switch', necessity: '', description: 'Switch to the joined document (only in the iroh console).'}
+    ],
+    examples: {
+      console: `> doc join --switch 6tcadaassjgjfmivyaycuads6ek4asma3qacdtvs6waaaaaaaaaanctrkxaetag4aaq45mxvqmruwqvq5l5vc4kvybeybxaaehhlf5mmh72ojerj4e2tcvoajganyabbz2zplnqhpyekxzhlfzyvlqcai55pmzg4d3x34mcpuydxoq4t5ec66zp3k3ouxwadxky745i3dwrhqcig3rqqdifkjjb3drfbo2krc7l3anoqly5wanom756kxmrqnap
+tiqpal5qnrb3idy7g4n7hnh5esex7zu6jtqyuwt6hr4iq2nnlpua`,
+    }
+  },
+  {
+    name: 'doc keys',  
+    description: 'List all keys in a document.',
+    slug: 'doc-keys',
+    arguments: [
+      { name: 'doc', necessity: 'required', description: 'Document to operate on. Required unless the document is set through the IROH_DOC environment variable. Within the iroh console, the active document can also set with `doc set`.'  },
+      { name: 'prefix', necessity: 'optional', description: 'Only list keys that start with prefix.' },
+      { name: 'author', necessity: 'optional', description: 'Filter by author.' },
+    ],
+    examples: {
+      console: `author:i3vpd4e7… doc:njszszvg…
+> doc set foo bar
+bafkr4ihs5cl65v6sa3gykxkecwmpuuq2xr22vfuvh2l4amgjmewdbqjjhu
+
+author:i3vpd4e7… doc:njszszvg…
+> doc set hello world
+bafkr4igxrffos4lnhdjn7lioyvkcjsrsd3qsiu6vd4ntvxvxpuchl3myrq
+
+author:i3vpd4e7… doc:njszszvg…
+> doc set good morning
+bafkr4idyxc27uxtcrzl5a3t42xu7hm3h2wn4mfaqt3ma5ummofsitynxle
+
+author:i3vpd4e7… doc:njszszvg…
+> doc keys
+@i3vpd4e7…: foo = 6lujp3wx… (3 B)
+@i3vpd4e7…: good = pc4ll6s6… (7 B)
+@i3vpd4e7…: hello = 26euv2lr… (5 B)
+
+# use a prefix to filter the keys
+
+author:i3vpd4e7… doc:njszszvg…
+> doc keys fo
+@i3vpd4e7…: foo = 6lujp3wx… (3 B)`,
+    }
+  },
+  {
+    name: 'doc list',
     description: 'List documents on this node.',
     slug: 'doc-list',
     examples: {
@@ -9,10 +181,6 @@ const doc = [
       tiqpal5qnrb3idy7g4n7hnh5esex7zu6jtqyuwt6hr4iq2nnlpua
       3ogcanavjfehmoeuf3jkel5pmbv2bpdwybvzt7xzk5sgbub72mia
       njszszvgpziwnxqnsi32nmc7j2czs2rnj3m7czavudurqxld3nbq`,
-      cli: `$ iroh doc list
-      tiqpal5qnrb3idy7g4n7hnh5esex7zu6jtqyuwt6hr4iq2nnlpua
-      3ogcanavjfehmoeuf3jkel5pmbv2bpdwybvzt7xzk5sgbub72mia
-      njszszvgpziwnxqnsi32nmc7j2czs2rnj3m7czavudurqxld3nbq`
     }
   },
   {
@@ -52,70 +220,6 @@ $ iroh doc 674deec7a19fec50fd6f486a5eef20509073ecf7c527b60a27c84baea90d3816 set 
     }
   },
   {
-    name: 'doc get',   
-    description: 'Get entries in a document.',
-    slug: 'doc-get',
-    arguments: [
-      { name: 'key', necessity: 'required', description: 'Key of the entry to fetch.' },
-      { name: 'doc_id', necessity: 'required', description: 'Required unless the document is set through the IROH_DOC environment variable. Within the Iroh console, the active document can also set with `doc set`.' },
-      { name: 'prefix', necessity: '', description: 'If true, get all entries that start with key.' },
-      { name: 'author', necessity: '', description: 'If provided, only return entries from this author.' },
-      { name: 'old', necessity: '', description: 'If true, old entries will be included. By default only the latest value for each key is shown.' },
-      { name: 'content', necessity: '', description: 'Also print the content for each entry (but only if smaller than 1MB and valid UTf-8).' }
-    ],
-    examples: {
-      console: `> doc new --switch
-dyyelvqqruxjwrlntsdvsksopwznmgnfxpehcsoqckklvqfxar2q
-Active doc is now dyyelvqq…
-
-author:fhu3uk4w… doc:dyyelvqq…
-> doc set foo bar
-@fhu3uk4w…: foo = 6lujp3wx… (3 B)
-
-author:fhu3uk4w… doc:dyyelvqq…
-> doc get foo -c
-@fhu3uk4w…: foo = 6lujp3wx… (3 B)
-bar
-
-`,
-    }
-  },
-  {
-    name: 'doc keys',  
-    description: 'List all keys in a document.',
-    slug: 'doc-keys',
-    arguments: [
-      { name: 'doc', necessity: 'required', description: 'Document to operate on. Required unless the document is set through the IROH_DOC environment variable. Within the iroh console, the active document can also set with `doc set`.'  },
-      { name: 'prefix', necessity: 'optional', description: 'Only list keys that start with prefix.' },
-      { name: 'author', necessity: 'optional', description: 'Filter by author.' },
-    ],
-    examples: {
-      console: `author:i3vpd4e7… doc:njszszvg…
-> doc set foo bar
-bafkr4ihs5cl65v6sa3gykxkecwmpuuq2xr22vfuvh2l4amgjmewdbqjjhu
-
-author:i3vpd4e7… doc:njszszvg…
-> doc set hello world
-bafkr4igxrffos4lnhdjn7lioyvkcjsrsd3qsiu6vd4ntvxvxpuchl3myrq
-
-author:i3vpd4e7… doc:njszszvg…
-> doc set good morning
-bafkr4idyxc27uxtcrzl5a3t42xu7hm3h2wn4mfaqt3ma5ummofsitynxle
-
-author:i3vpd4e7… doc:njszszvg…
-> doc keys
-@i3vpd4e7…: foo = 6lujp3wx… (3 B)
-@i3vpd4e7…: good = pc4ll6s6… (7 B)
-@i3vpd4e7…: hello = 26euv2lr… (5 B)
-
-# use a prefix to filter the keys
-
-author:i3vpd4e7… doc:njszszvg…
-> doc keys fo
-@i3vpd4e7…: foo = 6lujp3wx… (3 B)`,
-    }
-  },
-  {
     name: 'doc share', 
     description: 'Share a document with peers.',
     slug: 'doc-share',
@@ -139,19 +243,6 @@ gjfmivyaycuads6ek4asma3qacdtvs6waaaaaaaaaanctrkxaetag4aaq45mprsyystlwe66csxvqmru
   ` }
   },
   {
-    name: 'doc join',  
-    description: 'Join a document from a ticket.',
-    slug: 'doc-join',
-    arguments: [
-      { name: 'ticket', necessity: 'required', description: 'The ticket to join a document. Create a ticket with \'doc share\'.' },
-      { name: 'switch', necessity: '', description: 'Switch to the joined document (only in the iroh console).'}
-    ],
-    examples: {
-      console: `> doc join --switch 6tcadaassjgjfmivyaycuads6ek4asma3qacdtvs6waaaaaaaaaanctrkxaetag4aaq45mxvqmruwqvq5l5vc4kvybeybxaaehhlf5mmh72ojerj4e2tcvoajganyabbz2zplnqhpyekxzhlfzyvlqcai55pmzg4d3x34mcpuydxoq4t5ec66zp3k3ouxwadxky745i3dwrhqcig3rqqdifkjjb3drfbo2krc7l3anoqly5wanom756kxmrqnap
-tiqpal5qnrb3idy7g4n7hnh5esex7zu6jtqyuwt6hr4iq2nnlpua`,
-    }
-  },
-  {
     name: 'doc switch',
     description: 'Set the active document (only works within the Iroh console).',
     slug: 'doc-switch',
@@ -168,6 +259,23 @@ doc:ktrygcpx
 >`,
     }
   },
+  {
+    name: 'doc watch',  
+    description: 'Watch for changes and events on a document',
+    slug: 'doc-watch',
+    arguments: [
+      { name: 'doc', necessity: 'required', description: 'Document to operate on. Required unless the document is set through the IROH_DOC environment variable. Within the iroh console, the active document can also set with `doc set`.'  }
+    ],
+    examples: {
+      console: `author:i3vpd4e7… doc:njszszvg…
+  > doc set foo bar
+  bafkr4ihs5cl65v6sa3gykxkecwmpuuq2xr22vfuvh2l4amgjmewdbqjjhu
+
+  > doc watch
+  # events will show up here!`,
+    }
+  }
+  
 ]
 
 const author = [
