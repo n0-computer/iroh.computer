6839ca1935524971f4d302ed08bd551147c2d4c0
b5 <sparkle_pony_2000@qri.io>
2024-05-24 13:56:12 -0400

    revert to dig's layout for the moment

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,13 +5,13 @@ export async function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-gray">
-        <div tw="flex w-full">
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+        <div tw="bg-gray-50 flex w-full">
           <div tw="flex flex-col md:flex-row w-full py-12 px-4 m-8 md:items-center justify-between p-8">
-              <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-left">
-                <span className="text-purple-500">{searchParams.get('title')}</span>
-                <span tw="mt-5 text-lg sm:text-2xl text-gray-300 text-left">{searchParams.get('subtitle')}</span>
-              </h2>
+            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+              <span>{ searchParams.get('title') }</span>
+              <span tw="text-indigo-600">{ searchParams.get('subtitle') }</span>
+            </h2>
           </div>
         </div>
       </div>
