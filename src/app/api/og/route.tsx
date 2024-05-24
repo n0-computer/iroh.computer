42a7055a86b6e60d344125edd417a9b7b23b2295
b5 <sparkle_pony_2000@qri.io>
2024-05-24 13:41:34 -0400

    abandon custom background, dark mode + proper params

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,12 +5,12 @@ export async function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-iroh-kv-og-1 bg-white">
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-gray">
         <div tw="flex w-full">
           <div tw="flex flex-col md:flex-row w-full py-12 px-4 m-8 md:items-center justify-between p-8">
               <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-left">
-                <span className="text-purple-500">Title</span>
-                <span tw="mt-5 text-lg sm:text-2xl text-gray-500 text-left">Subtitle</span>
+                <span className="text-purple-500">{searchParams.get('title')}</span>
+                <span tw="mt-5 text-lg sm:text-2xl text-gray-300 text-left">{searchParams.get('subtitle')}</span>
               </h2>
           </div>
         </div>
