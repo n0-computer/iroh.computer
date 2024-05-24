a7747fb7959a8c4b15f7cef54ff3ad6d9b408676
b5 <sparkle_pony_2000@qri.io>
2024-05-24 12:58:21 -0400

    working on fog page

--- src/components/fog/FogHomepage.jsx
+++ src/components/fog/FogHomepage.jsx
@@ -9,17 +9,18 @@ import {
   ClipboardDocumentIcon,
   ClipboardDocumentCheckIcon
 } from '@heroicons/react/24/outline'
-import { Button } from './Button'
+import { Button } from '../Button'
 
 const koulen = localFont({
-  src: '../fonts/Koulen-Regular.ttf',
+  src: '../../fonts/Koulen-Regular.ttf',
   display: 'swap',
   variable: '--font-koulen-regular',
   weight: '200 900',
 })
 
+const install = `curl -fsSL https://iroh.computer/fog.sh | sh`
+
 export default function SendmePage() {
-  const install = `curl -fsSL https://iroh.computer/sendme.sh | sh`
   const [copied, setCopied] = React.useState(false)
 
   const handleCopy = () => {
@@ -34,9 +35,8 @@ export default function SendmePage() {
     <div className={clsx('w-full h-full bg-white text-zinc-700', koulen.variable)}>
       <div className="pt-10 mx-auto lg:max-w-5xl">
         <div className='max-w-5xl mx-auto text-center mt-20'>
-          <p className='text-lg text-zinc-500 font-koulen'>Need to send files? Try</p>
-          <h1 className='text-5xl lg:text-7xl font-koulen'>Sendme</h1>
-          <p className='text-lg text-zinc-500'>Free. No account required.</p>
+          <h1 className='text-5xl lg:text-7xl font-koulen'>FOG</h1>
+          <p className='text-lg text-zinc-500'>It's the cloud, but on the ground</p>
         </div>
         <Image src="/img/sendme/sendme_hero_1.svg" alt='one computer sending files to another computer through a pipe' width='1600' height='900' className='md:-mt-20' />
 
@@ -44,14 +44,20 @@ export default function SendmePage() {
         <div className='mx-5 md:mx-0 border-t border-l border-r shadow-sm'>
           <div className='md:flex'>
             <div className='px-5 py-10 border-b md:border-r md:w-5/12'>
-              <h2 className='text-4xl text-zinc-700 font-koulen'>File transfer<br />doesn&apos;t need to be complicated</h2>
-              <p className='mt-1 text-sm/6 text-gray-500'>It&apos;s like <span className='font-space-mono bg-zinc-100 px-1 py-0.5 rounded'>scp</span> without needing to know the IP address. Add some files to sendme, and it will give you a pastable ticket that you can give to anyone who needs your files. Sendme will connect your devices directly & transfer the data without any accounts or configuration.</p>
+              <h2 className='text-4xl text-zinc-700 font-koulen'>Run a dynamic Compute cluster from the laptop in your closet</h2>
+              <p className='mt-1 text-sm/6 text-gray-500'>Pull compute resources from <i className='italic'>anywhere</i> in a dynamic network that can run docker & WASM jobs. Data transfer, work scheduling, image pulling, and even discord integration are all handled automatically</p>
             </div>
 
             <div className='px-5 py-10 border-b flex-1 md:w-7/12'>
-              <h3 className='text-3xl font-koulen'>Install</h3>
-              <p className='mt-1 text-sm/6 text-gray-500'>Add sendme to your machine using shell:</p>
-              <button className='text-xs md:text rounded bg-zinc-100 p-2 mt-2 flex plausible-event-name=Sendme+Copy+Install+Script+Click' onClick={handleCopy}>
+              <h3 className='text-3xl font-koulen'>Fog bot</h3>
+              <p className='mt-1 text-sm/6 text-gray-500'>Join the iroh discord & use `/docker` in the #fog channel:</p>
+              <a href='https://iroh.computer/discord' className='text-xs md:text rounded bg-zinc-100 p-2 mt-2 flex plausible-event-name=Fog+Join+Discord+Click'>
+                JOIN DISCORD
+              </a>
+
+              <h3 className='text-3xl mt-10 font-koulen'>Install</h3>
+              <p className='mt-1 text-sm/6 text-gray-500'>Add fog to your machine using shell:</p>
+              <button className='text-xs md:text rounded bg-zinc-100 p-2 mt-2 flex plausible-event-name=Fog+Copy+Install+Script+Click' onClick={handleCopy}>
                 <div className='grow mr-10 font-spaceMono'>$ {install}</div>
                 {copied
                   ? <span className='w-10 mr-1'>copied!</span>
@@ -66,11 +72,11 @@ export default function SendmePage() {
 
         <div className='mx-5 md:mx-0 h-5 border-l border-r shadow-sm' />
         <div className='mx-5 md:mx-0 border-t border-l border-r shadow-sm'>
-          <div className='px-5 pt-10'>
-            <h3 className='font-koulen text-zinc-600 text-xl'>USING SENDME:</h3>
-          </div>
           <div className='md:flex sm:gap-5 border-b'>
             <div className='md:w-1/3 p-5 pb-10'>
+              <div className='px-5 pt-10'>
+                <h3 className='font-koulen text-zinc-600 text-xl'></h3>
+              </div>
               <div className='relative rounded border border-zinc-300 bg-zinc-100 p-2 py-8 h-40 mb-5'>
                 <div className='absolute top-0 left-0 p-2 flex gap-1'>
                   <div className='border border-zinc-400 w-3 h-3 rounded-full' />
@@ -81,10 +87,11 @@ export default function SendmePage() {
                 <p className='font-mono text-zinc-400 text-sm'>content added</p>
                 <p className='font-mono text-zinc-400 text-sm'>run sendme receive blobQmFoo...</p>
               </div>
-              <h3 className='font-koulen text-3xl'>1. Setup</h3>
+              <h3 className='font-koulen text-3xl'>Shared Workspace</h3>
               <p className='mt-1 text-sm/6 text-gray-500'>pass a file or folder you want to share to sendme. It&apos;ll spit out a ticket.</p>
             </div>
             <div className='md:w-1/3 p-5 pb-10'>
+              
               <div className='relative p-2 flex h-40 mb-5'>
                 <ChatBubbleLeftIcon className='w-6 h-6 mr-2' />
                 <div className='rounded-tr-lg rounded-br-lg rounded-bl-lg border bg-zinc-50 p-2 pb-10 mb-2 h-20'>
@@ -92,8 +99,8 @@ export default function SendmePage() {
                   blobQmFoo...
                 </div>
               </div>
-              <h3 className='font-koulen text-3xl'>2. Paste</h3>
-              <p className='mt-1 text-sm/6 text-gray-500'>copy-paste the ticket to your friend.</p>
+              <h3 className='font-koulen text-3xl'>S3 API support</h3>
+              <p className='mt-1 text-sm/6 text-gray-500'>every workspace stores all objects in a shared </p>
             </div>
             <div className='md:w-1/3 p-5 pb-10'>
               <div className='relative rounded border border-zinc-300 bg-zinc-100 p-2 py-8 h-40 mb-5'>
@@ -135,9 +142,9 @@ export default function SendmePage() {
             </div>
 
           <div className='border-t border-b bg-iroh-kv-1 p-5 py-20'>
-            <h2 className='text-2xl font-bold font-spaceMono'>Sendme is built on <a href="/" className='text-irohPurple-500'>iroh</a></h2>
-            <p className='md:max-w-lg mt-5'>Just like <a className='text-irohPurple-500' href="https://dumbpipe.dev">dumbpipe</a>, sendme is built on iroh. Sendme uses iroh <a className='text-irohPurple-700' href="/docs/layers/blobs">blobs</a> to send and verify your files, and <a className='text-irohPurple-700' href='/docs/layers/connections'>connections</a> to establish direct links between devices for data transfer</p>
-            <Button href='/' variant='filled' className='mt-5 plausible-event-name=Sendme+Iroh+CTA+Click'>BUILD ON IROH</Button>
+            <h2 className='text-2xl font-bold font-spaceMono'>Fog is built on <a href="/" className='text-irohPurple-500'>iroh</a></h2>
+            <p className='md:max-w-lg mt-5'>Just like <a className='text-irohPurple-500' href="https://iroh.computer/sendme">sendme</a> and <a className='text-irohPurple-500' href="https://dumbpipe.dev">dumbpipe</a>, sendme is built on iroh. Fog uses iroh <a className='text-irohPurple-700' href='https://iroh.computer/docs/layers/documents'>documents</a> to coordinate computer, <a className='text-irohPurple-700' href="/docs/layers/blobs">blobs</a> to replicate data, and <a className='text-irohPurple-700' href='/docs/layers/connections'>connections</a> to establish direct links between devices</p>
+            <Button href='/' variant='filled' className='mt-5 plausible-event-name=Fog+Iroh+CTA+Click'>BUILD ON IROH</Button>
           </div>
         </div>
         <div className='mx-5 mb-20 md:mx-0 h-5 border-l border-r' />
