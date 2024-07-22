a4d13f6a02f1d0be061e762a62098ed0df7fa8aa
dignifiedquire <me@dignifiedquire.com>
2024-07-22 19:00:31 +0200

    feat: update to 0.21

---
+++ api-code-examples/python/docs-create.py
@@ -0,0 +1,17 @@
+import iroh
+import asyncio
+
+
+async def main():
+    # Create in memory iroh node
+    node = await iroh.Iroh.memory()
+    node_id = await node.node().node_id()
+    print(f"Started Iroh node: {node_id}")
+
+    author = await node.authors().default()
+    print(f"Default author: {author}")
+
+    doc = await node.docs().create()
+    print(f"Created doc: {doc.id()}")
+
+asyncio.run(main())
