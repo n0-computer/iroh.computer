b0f42378c26010ff4a9b63aa39926cfd5af1bd87
dignifiedquire <me@dignifiedquire.com>
2024-08-16 15:15:08 +0200

    fixup minimal example

--- api-code-examples/nodejs/authors-create.js
+++ api-code-examples/nodejs/authors-create.js
@@ -1,2 +1,8 @@
+import { Iroh } from '@number0/iroh'
 
-console.log("this is how we author create")
+const node = await Iroh.memory()
+const nodeId = await node.node.nodeId()
+console.log(`Started iroh node: ${nodeId}`)
+
+const author = await node.authors.create()
+console.log(`Created author: ${author.toString()}`)
