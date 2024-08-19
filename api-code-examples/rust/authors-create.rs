d27011e0b845204e4431d1f021f57c8db085caff
Kasey Huizinga <ramfox@Kaseys-MBP.lan>
2024-08-19 18:44:22 -0400

    upgrade to iroh main

--- api-code-examples/rust/authors-create.rs
+++ api-code-examples/rust/authors-create.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let node = iroh::node::Node::memory().spawn().await?;
-    let node_id = node.node().node_id().await?;
+    let node_id = node.net().node_id().await?;
     println!("Started Iroh node: {node_id}");
 
     let author = node.authors().create().await?;
--- api-code-examples/rust/docs-create.rs
+++ api-code-examples/rust/docs-create.rs
@@ -3,7 +3,7 @@ async fn main() -> anyhow::Result<()> {
     // Create in memory iroh node
     let node = iroh::node::Node::memory().spawn().await?;
 
-    let node_id = node.node().node_id().await?;
+    let node_id = node.net().node_id().await?;
     println!("Started Iroh node: {node_id}");
 
     let author = node.authors().default().await?;
