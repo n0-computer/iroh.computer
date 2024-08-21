11bc578bb5c0cae2bb0732b4dd5524aa9e159421
b5 <sparkle_pony_2000@qri.io>
2024-08-22 01:13:23 +0200

    add docs get and docs drop for nodejs

--- api-code-examples/index.js
+++ api-code-examples/index.js
@@ -5,3 +5,7 @@ import './nodejs/blobs-add.js'
 import './nodejs/blobs-list-blobs.js'
 import './nodejs/blobs-list-collections.js'
 import './nodejs/blobs-list-incomplete-blobs.js'
+
+import './nodejs/docs-get.js'
+import './nodejs/docs-delete.js'
+import './nodejs/docs-drop.js'
---
+++ api-code-examples/nodejs/docs-drop.js
@@ -0,0 +1,21 @@
+import assert from 'assert'
+import { Iroh } from '@number0/iroh'
+
+const node = await Iroh.memory()
+const nodeId = await node.net.nodeId()
+console.log(`Started iroh node: ${nodeId}`)
+
+// create a doc
+const doc = await node.docs.create()
+console.log(`Created doc: ${doc.id()}`)
+
+// we have one document
+let docs = await node.docs.list()
+assert.equal(docs.length, 1)
+
+// delete the document
+await node.docs.dropDoc(doc.id())
+
+// we have zero documents
+docs = node.docs.list()
+assert.equal(docs.length, 0)
