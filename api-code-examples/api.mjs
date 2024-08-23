509a5fc03b57e5f7ad57610de971b14198652429
Divma <26765164+divagant-martian@users.noreply.github.com>
2024-08-22 20:36:24 -0500

    Revert "add and update blob commands (#176)"
    This reverts commit d6feb5d8fcff56612df44b3cd504ae196c7e6661.

--- api-code-examples/api.mjs
+++ api-code-examples/api.mjs
@@ -459,12 +459,36 @@ const blobs = [
     description: 'Add data from PATH to the running node',
     slug: 'blobs-add',
     arguments: [
-      { name: 'source', necessity: 'required', description: 'Path to a file or folder' },
-      { name: 'in-place', necessity: '', description: 'Add in place' },
-      { name: 'tag', necessity: '', description: 'Tag to tag the data with' },
-      { name: 'wrap', necessity: '', description: 'Wrap the added file or directory in a collection' },
-      { name: 'filename', necessity: '', description: 'Override the filename used for the entry in the created collection' },
-      { name: 'no-ticket', necessity: '', description: 'Do not print the all-in-one ticket to get the added data from this node' }
+      {
+        name: 'source',
+        necessity: 'required',
+        description: 'Path to a file or folder'
+      },
+      {
+        name: 'in-place',
+        necessity: '',
+        description: 'Add in place'
+      },
+      {
+        name: 'tag',
+        necessity: '',
+        description: 'Tag to tag the data with'
+      },
+      {
+        name: 'wrap',
+        necessity: '',
+        description: 'Wrap the added file or directory in a collection'
+      },
+      {
+        name: 'filename',
+        necessity: '',
+        description: 'Override the filename used for the entry in the created collection'
+      },
+      {
+        name: 'no-ticket',
+        necessity: '',
+        description: 'Do not print the all-in-one ticket to get the added data from this node'
+      }
     ],
     examples: {
       console: `> blobs add ~/my_txt.txt
@@ -472,27 +496,69 @@ Adding my_txt.txt as /Users/me/my_txt.txt...
 - /Users/me/my_txt.txt: 328 B bafkr4igef2yiz2nz33tljfdezzr45cos5lnc2urjmfi6zghbsnfzvpdpa4
 Total: 328 B
 
-Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
+Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`
     }
   },
   {
-    name: 'blobs download',  // console and cli blobs get
+    name: 'blobs download',
     description: 'Download data to the running node\'s database and provide it',
     slug: 'blobs-get',
     arguments: [
-      { name: 'ticket or hash', necessity: 'required', description: 'Ticket or Hash to use' },
-      { name: 'address', necessity: '', description: 'Additional socket address to use to contact the node. Can be used multiple times' },
-      { name: 'relay-url', necessity: '', description: 'Override the relay URL to use to contact the node' },
-      { name: 'recursive', necessity: '', description: 'Override to treat the blob as a raw blob or a hash sequence' },
-      { name: 'override-addresses', necessity: '', description: 'If set, the ticket\'s direct addresses will not be used' },
-      { name: 'node', necessity: '', description: 'NodeId of the provider' },
-      { name: 'out', necessity: '', description: 'Directory or file in which to save the file(s)' },
-      { name: 'stable', necessity: '', description: 'If set, the data will be moved to the output directory, and iroh will assume that it will not change' },
-      { name: 'tag', necessity: '', description: 'Tag to tag the data with' },
-      { name: 'queued', necessity: '', description: 'If set, will queue the download in the download queue' }
+      {
+        name: 'ticket or hash',
+        necessity: 'required',
+        description: 'Ticket or Hash to use'
+      },
+      {
+        name: 'address',
+        necessity: '',
+        description: 'Additional socket address to use to contact the node. Can be used multiple times'
+      },
+      {
+        name: 'relay-url',
+        necessity: '',
+        description: 'Override the relay URL to use to contact the node'
+      },
+      {
+        name: 'recursive',
+        necessity: '',
+        description: 'Override to treat the blob as a raw blob or a hash sequence'
+      },
+      {
+        name: 'override-addresses',
+        necessity: '',
+        description: 'If set, the ticket\'s direct addresses will not be used'
+      },
+      {
+        name: 'node',
+        necessity: '',
+        description: 'NodeId of the provider'
+      },
+      {
+        name: 'out',
+        necessity: '',
+        description: 'Directory or file in which to save the file(s)'
+      },
+      {
+        name: 'stable',
+        necessity: '',
+        description: 'If set, the data will be moved to the output directory, and iroh will assume that it will not change'
+      },
+      {
+        name: 'tag',
+        necessity: '',
+        description: 'Tag to tag the data with'
+      },
+      {
+        name: 'queued',
+        necessity: '',
+        description: 'If set, will queue the download in the download queue'
+      }
     ],
     examples: {
-      console: '> blobs get'
+      console: `> blobs get his7xcvl5jc734mwbtgtkkvxeafrdcitmg2jvdmxk7ri7abgr33a --address 192.168.0.15:41918 --node w3mj2iw47i2pgbdrnutaamsqm6h2bj6r7yp7h2vliruq3vry535a
+Fetching: his7xcvl5jc734mwbtgtkkvxeafrdcitmg2jvdmxk7ri7abgr33a
+Transferred 14.20 KiB in 0 seconds, 173.20 KiB/s`
     }
   },
   {
@@ -500,13 +566,29 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     description: 'Export a blob from the internal blob store to the local filesystem',
     slug: 'blobs-export',
     arguments: [
-      { name: 'hash', necessity: 'required', description: 'The hash to export' },
-      { name: 'out', necessity: 'required', description: 'Directory or file in which to save the file(s)' },
-      { name: 'recursive', necessity: '', description: 'Set to true if the hash refers to a collection and you want to export all children of the collection' },
-      { name: 'stable', necessity: '', description: 'If set, the data will be moved to the output directory, and iroh will assume that it will not change' }
+      {
+        name: 'hash',
+        necessity: 'required',
+        description: 'The hash to export'
+      },
+      {
+        name: 'out',
+        necessity: 'required',
+        description: 'Directory or file in which to save the file(s)'
+      },
+      {
+        name: 'recursive',
+        necessity: '',
+        description: 'Set to true if the hash refers to a collection and you want to export all children of the collection'
+      },
+      {
+        name: 'stable',
+        necessity: '',
+        description: 'If set, the data will be moved to the output directory, and iroh will assume that it will not change'
+      }
     ],
     examples: {
-      console: '> blobs export'
+      console: '> blobs export his7xcvl5jc734mwbtgtkkvxeafrdcitmg2jvdmxk7ri7abgr33a ./very_important.txt'
     }
   },
   {
@@ -527,7 +609,7 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
   },
   {
     name: 'blobs list incomplete-blobs',
-    description: 'List the available blobs on the running provider',
+    description: 'List the blobs on the running provider that are not full files',
     slug: 'blobs-list-incomplete-blobs',
     arguments: [],
     examples: {
@@ -540,7 +622,8 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     slug: 'blobs-list-collections',
     arguments: [],
     examples: {
-      console: '> blobs list collections'
+      console: `> blobs list collections
+"auto-2024-08-20T17:49:54.244Z": myumj2a42x6ky72erzt4djhu3ya46h2e7sp4ibqzew2ne4xnbpua 3 blobs (0 B)`
     }
   },
   {
@@ -548,8 +631,16 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     description: 'Validate hashes on the running node',
     slug: 'blobs-validate',
     arguments: [
-      { name: 'verbose', necessity: '', description: '' },
-      { name: 'repair', necessity: '', description: 'Repair the store by removing invalid data' }
+      {
+        name: 'verbose',
+        necessity: '',
+        description: ''
+      },
+      {
+        name: 'repair',
+        necessity: '',
+        description: 'Repair the store by removing invalid data'
+      }
     ],
     examples: {
       console: '> blobs validate'
@@ -560,8 +651,16 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     description: 'Perform a database consistency check on the running node',
     slug: 'blobs-consistency-check',
     arguments: [
-      { name: 'verbose', necessity: '', description: '' },
-      { name: 'repair', necessity: '', description: 'Repair the store by removing invalid data' }
+      {
+        name: 'verbose',
+        necessity: '',
+        description: ''
+      },
+      {
+        name: 'repair',
+        necessity: '',
+        description: 'Repair the store by removing invalid data'
+      }
     ],
     examples: {
       console: '> blobs consistency-check'
@@ -572,10 +671,13 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     description: 'Delete the given blobs',
     slug: 'blobs-delete-blob',
     arguments: [
-      { name: 'hash', necessity: 'required', description: 'Blobs to delete' }
+      {
+        name: 'hash',
+        necessity: 'required',
+        description: 'Blobs to delete'
+      }
     ],
     examples: {
-      console: '> blobs delete blob'
     }
   },
   {
@@ -583,12 +685,26 @@ Collection: bafkr4ie3xsx3vdsbflainnk6p4xs4h2hq3hdmuasuoflkgybvnsbljb3ke`,
     description: 'Get a ticket to share this blob',
     slug: 'blobs-share',
     arguments: [
-      { name: 'hash', necessity: 'required', description: 'Hash of the blob to share' },
-      { name: 'addr-options', necessity: '', description: 'Options to configure the address information in the generated ticket' },
-      { name: 'recursive', necessity: '', description: 'If the blob is a collection, the requester will also fetch the listed blobs' }
+      {
+        name: 'hash',
+        necessity: 'required',
+        description: 'Hash of the blob to share'
+      },
+      {
+        name: 'addr-options',
+        necessity: '',
+        description: 'Options to configure the address information in the generated ticket'
+      },
+      {
+        name: 'recursive',
+        necessity: '',
+        description: 'If the blob is a collection, the requester will also fetch the listed blobs'
+      }
     ],
     examples: {
-      console: '> blobs share'
+      console: `> blobs share myumj2a42x6ky72erzt4djhu3ya46h2e7sp4ibqzew2ne4xnbpua
+Ticket for blob myumj2a42x6ky72erzt4djhu3ya46h2e7sp4ibqzew2ne4xnbpua (96 B)
+blobac3nrhjc3t5dj4yeofwsmabskbty7ifh2h7b747kvncgsdowhdxpuaaaabtcrrhidtk7zld7ishgpqne6tpadty7it6j7ragdes3juts5uf6q`
     }
   }
 ]
