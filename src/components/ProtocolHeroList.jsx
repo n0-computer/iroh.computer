dc8e30a2823e2b8234712afaeaaf44563573034a
b5 <sparkle_pony_2000@qri.io>
2024-09-23 22:04:19 -0400

    drive protocols list with structured data

---
+++ src/components/ProtocolHeroList.jsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
+
+import {protocols} from '@/protocols'
+import {GlowCard} from '@/components/GlowCard';
+
+import iconCarat from '@/images/icons/icon_carat.svg';
+import iconFaster from '@/images/icons/icon_faster.svg';
+import iconOpen from '@/images/icons/icon_open.svg';
+import iconPlatforms from '@/images/icons/icon_platforms.svg';
+
+const icons = {
+  iconCarat,
+  iconFaster,
+  iconOpen,
+  iconPlatforms
+}
+
+export const ProtocolHeroList = function ProtocolHeroList() {
+  return (
+    <div className='md:grid md:grid-cols-4 border-l border-t border-irohGray-300 dark:border-irohGray-800'>
+      {protocols.map((proto, i) => (
+        <GlowCard key={i} className='border-r border-b border-irohGray-300 dark:border-irohGray-800 p-5'>
+          <Image
+            src={icons[proto.icon]}
+            alt=""
+            className="h-12 w-12 float-left md:float-none"
+            unoptimized
+            />
+          <div className='pl-20 md:pl-0'>
+            <h2 className='text-2xl mb-2 md:mt-10 text-irohPurple-500 font-bold'>{proto.title}</h2>
+            <p className=''>{proto.tagline}</p>
+          </div>
+        </GlowCard>
+      ))}
+      <GlowCard className='border-r border-b border-irohGray-300 dark:border-irohGray-800 p-5'>
+        <h1 className='text-2xl mb-2 text-irohGray-500'>Browse all protocols</h1>
+        <ArrowRightIcon className='w-10 h-10' />
+      </GlowCard>
+    </div>
+  )
+}
---
+++ src/protocols.js
@@ -0,0 +1,32 @@
+export const protocols = [
+  {
+    "icon": "iconOpen",
+    "title": "Blobs",
+    "tagline": "Resumable, verifiable data transfer"
+  },
+  {
+    "icon": "iconPlatforms",
+    "title": "Gossip",
+    "tagline": "Broadcast messages to groups of nodes by topic."
+  },
+  // {
+  //   "icon": "iconOpen",
+  //   "title": "DMs",
+  //   "tagline": "Simple node-to-node message passing"
+  // },
+  {
+    "icon": "iconFaster",
+    "title": "Documents",
+    "tagline": "Realtime, multiwriter, key-value sync"
+  },
+  // {
+  //   "icon": "iconFaster",
+  //   "title": "Willow",
+  //   "tagline": "multiwriter, key-value sync with fine-grained access control"
+  // },
+  // {
+  //   "icon": "iconFaster",
+  //   "title": "Automerge",
+  //   "tagline": "CRDT-powered collaborative documents"
+  // }
+]
