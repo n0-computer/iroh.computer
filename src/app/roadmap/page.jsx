a3c3b564e95b44242a919f8fff68eb1a2d61e3bc
b5 <sparkle_pony_2000@qri.io>
2024-10-24 10:23:14 -0400

    make pretty

--- src/app/roadmap/page.jsx
+++ src/app/roadmap/page.jsx
@@ -1,17 +1,31 @@
+import Link from 'next/link';
+import clsx from 'clsx';
 import { CheckCircleIcon } from '@heroicons/react/20/solid';
+
 import roadmap from './roadmap.json';
 import { BlankLayout } from '@/components/BlankLayout';
+import { formatDate } from '@/lib/formatDate'
 
 export const metadata = {
   title: 'Roadmap',
   description:
-    'Sync Anywhere',
+    'Iroh 1.0 roadmap. Here\'s where we\'re headed, and progress we made against our goal to date.',
 };
 
 
 export default function Component() {
+  const { last_updated } = roadmap;
   return (
     <BlankLayout>
+        <header className="max-w-lg px-4 sm:px-0">
+          <h1 className="text-2xl sm:text-4xl font-space font-bold tracking-tight text-zinc-800 dark:text-zinc-100">iroh 1.0 roadmap</h1>
+          <p className="my-3 text-base text-zinc-600 dark:text-irohGray-400">Here's where we're headed, and progress we made against our goal to date. For more details see the <Link className='text-irohPurple-500 underline decoration-dotted' href="/blog/road-to-1.0">iroh 1.0 roadmap post</Link></p>
+          <p className='font-space tracking-tight text-zinc-800 dark:text-zinc-100'>Last Updated {formatDate(last_updated)}</p>
+        </header>
+        <div className='max-w-2xl mb-20'>
+          <h1 className='text-3xl font-bold font-spaceMono text-irohGray-700 mb-4'></h1>
+          <p className='text-irohGray-500'></p>
+        </div>
         {roadmap.milestones.map((milestone, i) => {
           if (milestone.version) {
             return <Release key={i} data={milestone} />
@@ -23,14 +37,21 @@ export default function Component() {
 }
 
 function Release({ data }) {
-  const { version, done } = data;
+  const { version, done, doc, released, expected } = data;
   return (
-    <div className='pr-5 ml-0.5 py-8 relative w-32 border-r text-right'>
-      {done
-        ? <CheckCircleIcon className='w-7 h-7 text-irohPurple-500 absolute -right-3.5 top-8' />
-        : <CheckCircleIcon className='w-7 h-7 text-irohGray-200 absolute -right-3.5 top-8' />}
+    <div className={clsx('pr-5 ml-calc pt-8 pb-2 relative w-32 border-r dark:border-irohGray-400 text-right', done && 'border-irohPurple-500 dark:border-irohPurple-500')}>
+      <div className='absolute -right-3.5 top-8 w-7 h-7 rounded-full bg-white dark:bg-irohGray-900'>
+        {done
+          ? <CheckCircleIcon className='w-7 h-7 text-irohPurple-500' />
+          : <CheckCircleIcon className='w-7 h-7 text-irohGray-200 dark:text-irohGray-400' />}
+      </div>
       <div>
-        <h2 className='text-xl font-spaceMono font-bold'>{version}</h2>
+        <h2 className='text-lg font-space font-bold tracking-tight text-irohGray-400'>
+        {doc
+          ? <Link className='text-irohPurple-500 cursor-pointer' href={doc}>{version}</Link>
+          : version }</h2>
+        {released && <p className='text-xs text-irohGray-700'>{formatDate(released, true)}</p>}
+        {expected && <p className='text-xs text-irohGray-400'>ETA {expected}</p>}
       </div>
     </div>
   )
@@ -40,13 +61,18 @@ function Milestone({ data }) {
   const { done, title, description, subtasks, tracking_issue, doc } = data;
   const issueNunber = tracking_issue?.split("/").slice(-1)[0]
   return (
-    <div className='border-l ml-32 relative pb-5 max-w-lg'>
-      {done
-        ? <CheckCircleIcon className='w-5 h-5 text-irohPurple-500 absolute -left-2.5 top-1' />
-        : <CheckCircleIcon className='w-5 h-5 text-irohGray-200 absolute -left-2.5 top-1' />}
-      <div className='px-4'>
-        <h3 className='text-xl font-space font-bold'>{title}</h3>
-        <p className='text-sm font-spaceMono text-irohGray-500'>{description}</p>
+    <div style={{ marginLeft: '7.95rem' }} className={clsx(
+      'border-l relative pb-5 max-w-md dark:border-irohGray-700', 
+      done && 'border-irohPurple-500 dark:border-irohPurple-500'
+    )}>
+      <div className='absolute -left-2.5 rounded-full bg-white dark:bg-irohGray-900'>
+        {done
+          ? <CheckCircleIcon className='w-5 h-5 text-irohPurple-500' />
+          : <CheckCircleIcon className='w-5 h-5 text-irohGray-600 dark:text-irohGray-400' />}
+      </div>
+      <div className='px-4 pb-3'>
+        <h3 style={{ lineHeight: 0.8 }} className='text-xl text-irohGray-600 font-space font-bold dark:text-irohGray-400'>{title}</h3>
+        <p className='text-sm leading tight font-space text-irohGray-500'>{description}</p>
         {tracking_issue && <a href={tracking_issue} className='text-md underline mr-4'>#{issueNunber}</a>}
         {doc && <a href={doc} className='text-md underline'>details</a>}
         <Subtasks data={subtasks} />
@@ -64,8 +90,8 @@ function Subtasks({ data }) {
         <li key={i}>
           {data[taskName]
             ? <CheckCircleIcon className='w-4 h-4 text-irohPurple-500 inline-block' />
-            : <CheckCircleIcon className='w-4 h-4 text-irohGray-200 inline-block' />}
-          <span className='ml-2 font-spaceMono text-irohGray-500'>{taskName}</span>
+            : <CheckCircleIcon className='w-4 h-4 text-irohGray-200 dark:text-irohGray-400 inline-block' />}
+          <span className='ml-2 leading-tight text-xs font-spaceMono text-irohGray-500'>{taskName}</span>
         </li>
       ))}
     </ul>
