7eef5e4ca7bba5a05b459b58067798956fe58058
Brendan O'Brien <sparkle_pony_2000@qri.io>
2024-10-25 11:40:40 -0400

    roadmap page (#214)
    * roadmap page
    * align v0.28.0 release plan
    * make pretty
    * lint
    * fix dark mode release line
    * fix up dates
    * update roadmap
    * fix milestone leading
    * add party at the end of the roadmap
    * add ellipsis to show time break
    * add link to v0.27.0 blog post
    * it's not STUN over QUIC, it's QUIC Address Discovery

---
+++ src/app/roadmap/page.jsx
@@ -0,0 +1,138 @@
+import Link from 'next/link';
+import clsx from 'clsx';
+import { CheckCircleIcon, EllipsisHorizontalIcon, TrophyIcon } from '@heroicons/react/20/solid';
+
+import roadmap from './roadmap.json';
+import { BlankLayout } from '@/components/BlankLayout';
+import { formatDate } from '@/lib/formatDate'
+
+export const metadata = {
+  title: 'Roadmap',
+  description:
+    'Iroh 1.0 roadmap. Here\'s where we\'re headed, and progress we made against our goal to date.',
+};
+
+
+export default function Component() {
+  const { last_updated } = roadmap;
+  return (
+    <BlankLayout>
+        <header className="max-w-lg px-4 sm:px-0">
+          <h1 className="text-2xl sm:text-4xl font-space font-bold tracking-tight text-zinc-800 dark:text-zinc-100">iroh 1.0 roadmap</h1>
+          <p className="my-3 text-base text-zinc-600 dark:text-irohGray-400">Here&apos;s where we&apos;re headed, and progress we made against our goal to date. For more details see the <Link className='text-irohPurple-500 underline decoration-dotted' href="/blog/road-to-1.0">iroh 1.0 roadmap post</Link></p>
+          <p className='font-space tracking-tight text-zinc-800 dark:text-zinc-100'>Last Updated {formatDate(last_updated)}</p>
+        </header>
+        <div className='max-w-2xl mb-20'>
+          <h1 className='text-3xl font-bold font-spaceMono text-irohGray-700 mb-4'></h1>
+          <p className='text-irohGray-500'></p>
+        </div>
+        {roadmap.milestones.map((milestone, i) => {
+          if (milestone.version) {
+            return <Release key={i} data={milestone} />
+          } else if (milestone.all_done === false || milestone.all_done === true) {
+            return <AllDone key={i} data={milestone} />
+          } else if (milestone.ellipsis) {
+            return <Ellipsis key={i} data={milestone} />
+          }
+          return <Milestone key={i} data={milestone} />
+        })}
+    </BlankLayout>
+  )
+}
+
+function Release({ data }) {
+  const { version, done, doc, released, expected } = data;
+  return (
+    <div className={clsx(
+      'pr-5 ml-calc pt-9 pb-2 relative w-32 border-r dark:border-irohGray-700 text-right', 
+      done && 'border-irohPurple-500 dark:border-irohPurple-500')}>
+      <div className='absolute -right-3.5 top-8 w-7 h-7 rounded-full bg-white dark:bg-irohGray-900'>
+        {done
+          ? <CheckCircleIcon className='w-7 h-7 text-irohPurple-500' />
+          : <CheckCircleIcon className='w-7 h-7 text-irohGray-200 dark:text-irohGray-400' />}
+      </div>
+      <div>
+        <h2 className='text-lg font-space font-bold tracking-tight leading-tight text-irohGray-400'>
+        {doc
+          ? <Link className='text-irohPurple-500 cursor-pointer' href={doc}>{version}</Link>
+          : version }</h2>
+        {released && <p className='text-xs text-irohGray-400 dark:text-irohGray-600'>{formatDate(released, true)}</p>}
+        {expected && <p className='text-xs text-irohGray-400 dark:text-irohGray-600'>{expected}</p>}
+      </div>
+    </div>
+  )
+}
+
+function Milestone({ data }) {
+  const { done, title, description, subtasks, tracking_issue, doc } = data;
+  const issueNunber = tracking_issue?.split("/").slice(-1)[0]
+  return (
+    <div style={{ marginLeft: '7.95rem' }} className={clsx(
+      'border-l relative pb-5 max-w-md dark:border-irohGray-700', 
+      done && 'border-irohPurple-500 dark:border-irohPurple-500'
+    )}>
+      <div className='absolute -left-2.5 rounded-full bg-white dark:bg-irohGray-900'>
+        {done
+          ? <CheckCircleIcon className='w-5 h-5 text-irohPurple-500' />
+          : <CheckCircleIcon className='w-5 h-5 text-irohGray-600 dark:text-irohGray-400' />}
+      </div>
+      <div className='px-4 pb-3'>
+        <h3 className='text-xl mb-1 leading-6 text-irohGray-600 font-space font-bold dark:text-irohGray-400'>{title}</h3>
+        <p className='text-sm leading-5 font-space text-irohGray-500'>{description}</p>
+        {tracking_issue && <a href={tracking_issue} className='text-md underline mr-4'>#{issueNunber}</a>}
+        {doc && <a href={doc} className='text-md underline'>details</a>}
+        <Subtasks data={subtasks} />
+      </div>
+    </div>
+  )
+}
+
+function Subtasks({ data }) {
+  if (!data) return null;
+
+  return (
+    <ul className='pl-3 mt-2'>
+      {Object.keys(data).map((taskName, i) => (
+        <li key={i}>
+          {data[taskName]
+            ? <CheckCircleIcon className='w-4 h-4 text-irohPurple-500 inline-block' />
+            : <CheckCircleIcon className='w-4 h-4 text-irohGray-200 dark:text-irohGray-400 inline-block' />}
+          <span className='ml-2 leading-tight text-xs font-spaceMono text-irohGray-500'>{taskName}</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function Ellipsis() {
+  return (
+    <div style={{ marginLeft: '7.95rem' }} className='relative py-10 border-l max-w-md dark:border-irohGray-700'>
+      <div className='absolute -left-2.5 rounded-full bg-white dark:bg-irohGray-900'>
+        <EllipsisHorizontalIcon className='w-5 h-5 text-irohGray-600 dark:text-irohGray-400' />
+      </div>
+      <div className='px-4 pb-3'>
+        <p className='text-sm leading-5 font-space italic text-irohGray-500'>future work</p>
+      </div>
+    </div>
+  )
+}
+
+function AllDone({ data }) {
+  const { title, description, link, all_done } = data;
+  return (
+    <div style={{ marginLeft: '7.95rem' }} className={clsx(
+      'relative pb-5 max-w-md dark:border-irohGray-700', 
+      all_done && 'border-irohPurple-500 dark:border-irohPurple-500'
+    )}>
+      <div className='absolute -left-2.5 rounded-full bg-white dark:bg-irohGray-900'>
+        <Link href={link}>
+          <TrophyIcon className='w-5 h-5 text-irohGray-600 dark:text-irohGray-400' />
+        </Link>
+      </div>
+      <div className='px-4 pb-3'>
+        <h3 className='text-xl mb-1 leading-6 text-irohGray-600 font-space font-bold dark:text-irohGray-400'>{title}</h3>
+        <p className='text-sm leading-5 font-space text-irohGray-500'>{description}</p>
+      </div>
+    </div>
+  )
+}
--- src/components/BlogPostLayout.jsx
+++ src/components/BlogPostLayout.jsx
@@ -1,9 +1,5 @@
 'use client'
 
-import { useContext } from 'react'
-import { useRouter } from 'next/navigation'
-
-import { AppContext } from '@/app/providers'
 import { Prose } from '@/components/Prose'
 import { formatDate } from '@/lib/formatDate'
 import References from '@/components/References'
@@ -11,23 +7,7 @@ import BlogHeader from '@/components/BlogHeader'
 import { FooterMarketing } from './FooterMarketing'
 import { ChevronLeftIcon } from '@heroicons/react/20/solid'
 
-function ArrowLeftIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
-
 export function BlogPostLayout({ article, references = [], children }) {
-  let router = useRouter()
-  let { previousPathname } = useContext(AppContext)
-
   return (
     <div>
       <BlogHeader />
