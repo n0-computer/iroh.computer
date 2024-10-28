8ef3bdbf3134caa9fba6740ef65466c47a8265e4
b5 <sparkle_pony_2000@qri.io>
2024-10-27 22:52:34 -0400

    add real sitemap, rss reader

---
+++ src/app/rss/route.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+import RSS from "rss";
+
+import { getAllPosts } from "@/lib/blog";
+
+export async function GET() {
+  const allPosts = await getAllPosts();
+
+  const site_url =
+  process.env.NODE_ENV === "production"
+    ? "https://iroh.computer"
+    : "http://localhost:3000";
+
+  const feedOptions = {
+    title: "Blog posts | RSS Feed",
+    description: "Welcome to this blog posts!",
+    site_url: site_url,
+    feed_url: `${site_url}/rss.xml`,
+    image_url: `${site_url}/img/logo/iroh-wordmark-purple.svg`,
+    pubDate: new Date(),
+    copyright: `All rights reserved ${new Date().getFullYear()}`,
+  };
+
+  const feed = new RSS(feedOptions);
+
+  // Add each individual post to the feed.
+  allPosts.map((post) => {
+    feed.item({
+      title: post.title,
+      description: post.excerpt,
+      url: `${site_url}/blog/${post.slug}`,
+      date: post.date,
+    });
+  });
+
+  // Write the RSS feed to a file as XML.
+  const feedString = feed.xml({ indent: true })
+  return new Response(feedString, {
+    status: 200,
+    headers: { "Content-Type": "application/rss+xml" },
+  });
+};
