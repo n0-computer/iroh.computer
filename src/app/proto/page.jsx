d716ef14ae09df9e70bc636fb38780675decc0d5
b5 <sparkle_pony_2000@qri.io>
2024-12-02 17:03:38 -0500

    fix

--- src/app/proto/page.jsx
+++ src/app/proto/page.jsx
@@ -18,12 +18,12 @@ export default function Page() {
       <HeroPattern />
       <Prose>
         <Heading size={1}>Protocols</Heading>
-        <p>Iroh Protocols are pluggable extensions that build on direct connections. They're like HTTP server middleware on steriods! Mix & match for fast, flexible, functionality.</p>
+        <p>Iroh Protocols are pluggable extensions that build on direct connections. Like HTTP server middleware on steriods! Mix & match for fast, flexible, functionality.</p>
       </Prose>
       <div className="not-prose">
         <div className="mb-16 mt-6 flex gap-3">
-          <Button href="/docs/protocols" arrow="right" children="Protocols Overview" />
-          <Button href="/docs/protocol-quickstart" variant="outline" children="Write a protocol" />
+          <Button href="/docs/protocols" arrow="right">Protocols Overview</Button>
+          <Button href="/docs/protocol-quickstart" variant="outline">Write a protocol</Button>
         </div>
       </div>
       <ProtocolHeroList />
