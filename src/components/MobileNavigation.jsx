a8594d62232aeb30e65423b1b780962fcf947d4e
Brendan O'Brien <sparkle_pony_2000@qri.io>
2025-02-07 16:22:14 -0500

    fix: docs navigation on mobile (#290)

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -50,7 +50,7 @@ export const useMobileNavigationStore = create((set) => ({
   toggle: () => set((state) => ({isOpen: !state.isOpen})),
 }));
 
-export function MobileNavigation() {
+export function MobileNavigation({ sidebar = [] }) {
   const isInsideMobileNavigation = useIsInsideMobileNavigation();
   const {isOpen, toggle, close} = useMobileNavigationStore();
   const ToggleIcon = isOpen ? XIcon : MenuIcon;
@@ -131,7 +131,7 @@ export function MobileNavigation() {
                   layoutScroll
                   className="fixed bottom-0 left-0 top-14 w-full overflow-y-auto bg-white px-4 pb-4 pt-6 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/7.5 dark:bg-zinc-900 dark:ring-zinc-800 min-[416px]:max-w-sm sm:px-6 sm:pb-10"
                 >
-                  <Navigation />
+                  <Navigation sidebar={sidebar} />
                 </motion.div>
               </Transition.Child>
             </Dialog.Panel>
