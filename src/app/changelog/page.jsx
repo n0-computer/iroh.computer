4070d6117ccdbed3325029afa31ce40a3ac132b1
b5 <sparkle_pony_2000@qri.io>
2025-02-17 14:47:11 -0500

    fill out changelog details

--- src/app/changelog/page.jsx
+++ src/app/changelog/page.jsx
@@ -24,13 +24,13 @@ export default function Component() {
           <p>We&apos;ll send you an email with each new release of iroh &amp; and a link to the blog post. Right now iroh is releasing roughly every 3 weeks.</p>
         </div>
       </div>
-      {releases.map((release, i) => <Release key={i} data={release} />)}
+      {releases.map((release, i) => <Release key={release.version} data={release} />)}
     </BlankLayout>
   )
 }
 
 function Release({ data }) {
-  const { version, date, title, github_release, changelog, blog_post } = data;
+  const { version, date, title, github_release, changelog, blog_post, highlights } = data;
   return (
     <div style={{ marginLeft: '7.95rem' }} className={clsx(
       'border-l relative pb-8 max-w-md dark:border-irohGray-700 border-irohPurple-500',
@@ -45,10 +45,13 @@ function Release({ data }) {
         <p className='text-xs text-irohGray-400 dark:text-irohGray-600'>{formatDate(date, true)}</p>
       </div>
       <div className='px-4 pb-3'>
-        <h3 className={clsx('text-xl mb-1 leading-6 text-irohGray-600 font-space font-bold dark:text-irohGray-400', !title && "opacity-20")}>{title || "untitled"}</h3>
-        {blog_post && <a href={blog_post} className='text-md underline mr-4'>blog post</a>}
+        <h3 className={clsx('text-xl mb-1 leading-6 text-irohGray-800 font-space font-bold dark:text-irohGray-100', !title && "opacity-20")}>{title || "untitled"}</h3>
+        {blog_post && <Link href={blog_post} className='text-md underline mr-4'>blog post</Link>}
         {github_release && <a href={github_release} className='text-md underline mr-4'>github</a>}
         {changelog && <a href={changelog} className='text-md underline'>changelog</a>}
+        {highlights && (<ul className='list-disc ml-5 mt-2 text-sm'>
+          {highlights.map((hl, i) => <li className='opacity-50' key={i}>{hl}</li>)}
+        </ul>)}
       </div>
     </div>
   )
