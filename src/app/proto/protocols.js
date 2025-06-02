b3b09097d360b64b06cfd024eb8d2a778711bbcd
b5 <sparkle_pony_2000@qri.io>
2025-06-02 14:55:46 -0400

    add iroh-roq protocol

--- src/app/proto/protocols.js
+++ src/app/proto/protocols.js
@@ -7,7 +7,7 @@ export const protocols = [
     "slug": "iroh-blobs",
     "repository": "https://github.com/n0-computer/iroh-blobs",
     "documentation": "https://docs.rs/iroh-blobs/latest/iroh_blobs/",
-    "version": "v0.33.0" // TODO - pull version from github
+    "version": "v0.35.0" // TODO - pull version from github
   },
   {
     "featured": 2,
@@ -17,7 +17,7 @@ export const protocols = [
     "slug": "iroh-gossip",
     "repository": "https://github.com/n0-computer/iroh-gossip",
     "documentation": "https://docs.rs/iroh-gossip/latest/iroh_gossip/",
-    "version": "v0.33.0" // TODO - pull version from github
+    "version": "v0.35.0" // TODO - pull version from github
   },
   {
     "featured": 3,
@@ -27,6 +27,15 @@ export const protocols = [
     "slug": "iroh-docs",
     "documentation": "https://docs.rs/iroh-docs/latest/iroh_docs/",
     "repository": "https://github.com/n0-computer/iroh-docs",
-    "version": "v0.33.0" // TODO - pull version from github
+    "version": "v0.35.0" // TODO - pull version from github
+  },
+  {
+    "icon": "",
+    "title": "iroh-roq",
+    "tagline": "RTP over QUIC implemented as iroh protocol",
+    "slug": "iroh-roq",
+    "documentation": "https://docs.rs/iroh-roq/latest/iroh_roq/",
+    "repository": "https://github.com/n0-computer/iroh-roq",
+    "version": "v0.1.0"
   }
 ]
