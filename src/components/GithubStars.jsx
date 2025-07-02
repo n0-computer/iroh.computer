f6b4c43a409ff95ecd5d18801dc8bf07455406bf
b5 <sparkle_pony_2000@qri.io>
2025-07-02 16:17:45 -0400

    docs: add NodeAddr page

--- src/components/GithubStars.jsx
+++ src/components/GithubStars.jsx
@@ -6,7 +6,7 @@ export default function GithubStars(props) {
   return (
     <Link href="https://github.com/n0-computer/iroh" className='p-2 -mt-2 flex text-sm leading-5 fill-zinc-400 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-600 dark:hover:fill-zinc-600 hover:bg-black/10 rounded'>
       <GithubIcon className="h-5 w-5" />
-      <span className='ml-2 mt-0'>4.6k</span>
+      <span className='ml-2 mt-0'>5.4k</span>
     </Link>
   )
 }
