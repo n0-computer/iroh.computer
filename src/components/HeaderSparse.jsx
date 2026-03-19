7b2092b62809340e73102aacdf3def53c09271c4
Rae McKelvey <633012+okdistribute@users.noreply.github.com>
2026-03-19 09:53:53 -0700

    fix signup button

--- src/components/HeaderSparse.jsx
+++ src/components/HeaderSparse.jsx
@@ -135,13 +135,6 @@ export function HeaderSparse() {
                 <li>
                   <GithubStars />
                 </li>
-                <li>
-                  <Link
-                    href="https://iroh.computer/discord"
-                    className='px-3 py-2 -mt-2 flex text-sm bg-zinc-200 text-zinc-600 dark:bg-zinc-800 dark:text-zinc-100 transition hover:text-irohPurple-500 dark:hover:text-irohPurple-500 hover:bg-black/10 rounded'>
-                      Signup
-                  </Link>
-                </li>
               </ul>
             </div>
           </div>
